4f282c33182aa5e8894ce14a18e0a6b5ca3f954e
Sunnyraj8789 <141814845+Sunnyraj8789@users.noreply.github.com>
2023-11-18 22:51:15 +0530

    Update Without.java

--- Without.java
+++ Without.java
@@ -1,1 +1,12 @@
 
+public class without {
+    public static void obj()
+    {
+        System.out.println("static method");
+    }
+    public static void main(String[] args)
+    {
+        without.obj();
+    }
+    
+}
